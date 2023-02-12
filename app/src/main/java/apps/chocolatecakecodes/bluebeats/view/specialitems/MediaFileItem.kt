c68f75b67d85d5afa0b51bb7c2b0ca8851dc317b
blued_gear <chocolatecakecodes@disroot.org>
2023-02-12 15:57:45 +0800

    [T56] extend MediaFileItem with thumbnails

--- app/src/main/java/apps/chocolatecakecodes/bluebeats/view/specialitems/MediaFileItem.kt
+++ app/src/main/java/apps/chocolatecakecodes/bluebeats/view/specialitems/MediaFileItem.kt
@@ -1,10 +1,17 @@
 package apps.chocolatecakecodes.bluebeats.view.specialitems
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.doOnNextLayout
 import apps.chocolatecakecodes.bluebeats.R
+import apps.chocolatecakecodes.bluebeats.media.VlcManagers
 import apps.chocolatecakecodes.bluebeats.media.model.MediaFile
 import com.mikepenz.fastadapter.drag.IDraggable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * @param file the MediaFile to represent
@@ -14,7 +21,8 @@ import com.mikepenz.fastadapter.drag.IDraggable
 internal open class MediaFileItem(
     val file: MediaFile,
     override val isDraggable: Boolean = false,
-    private val useTitle: Boolean = false
+    private val useTitle: Boolean = false,
+    private val showThumb: Boolean = false
 ) : SelectableItem<MediaFileItem.ViewHolder>(), IDraggable {
 
     override val type: Int = MediaFileItem::class.hashCode()
@@ -27,6 +35,7 @@ internal open class MediaFileItem(
 
         private val title: TextView = view.findViewById(R.id.v_mf_text)
         private val dragHandle: View = view.findViewById(R.id.v_mf_handle)
+        private val thumb: ImageView = view.findViewById(R.id.v_mf_thumb)
 
         override fun bindView(item: MediaFileItem, payloads: List<Any>) {
             super.bindView(item, payloads)
@@ -35,12 +44,42 @@ internal open class MediaFileItem(
                     item.file.mediaTags.title
                 else
                     item.file.name
+
             dragHandle.visibility = if(item.isDraggable) View.VISIBLE else View.GONE
+
+            if(item.showThumb) {
+                thumb.visibility = View.VISIBLE
+
+                if(thumb.width == 0 || thumb.height == 0){
+                    thumb.doOnNextLayout {
+                        loadThumbnail(item.file)
+                    }
+                } else {
+                    loadThumbnail(item.file)
+                }
+            } else {
+                thumb.visibility = View.GONE
+            }
         }
         override fun unbindView(item: MediaFileItem) {
             super.unbindView(item)
 
             title.text = null
+            thumb.setImageBitmap(null)
+        }
+
+        private fun loadThumbnail(file: MediaFile) {
+            //TODO loading placeholder, no-thumb placeholder
+            if(thumb.height > 0) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    VlcManagers.getMediaDB().getSubject()
+                        .getThumbnail(file, -1, thumb.height).let {
+                            withContext(Dispatchers.Main) {
+                                thumb.setImageBitmap(it)
+                            }
+                        }
+                }
+            }
         }
     }
 }
