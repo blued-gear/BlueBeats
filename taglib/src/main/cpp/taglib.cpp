2b9ec967f86e6f930618615a36cadd6f6b277123
blued_gear <chocolatecakecodes@disroot.org>
2023-02-12 15:57:42 +0800

    changes struct JavaIDs usage

--- taglib/src/main/cpp/taglib.cpp
+++ taglib/src/main/cpp/taglib.cpp
@@ -10,33 +10,10 @@ using TagLib::String;
 
 //region forward declarations
 namespace {
-    namespace BlueBeats {
-        // aliases for better type-checking
-        typedef jobject TagFields;
-        typedef jobject UserTags;
-
-        void throwParseException(JNIEnv *jni, const char *msg);
-
-        TagFields readTags(JNIEnv *jni, const File &file);
-
-        void readID3Tags(JNIEnv *jni, const Tag *tag, const Properties* audioProps, TagFields dest);
-    }
-
     /// struct to cache all handles to java classes, fields and methods
     struct JavaIDs_t{
-        jclass c_ParseException = nullptr;
 
-        jclass c_TagParser = nullptr;
-        jfieldID f_TagParser_TagFields = nullptr;
-        jfieldID f_TagParser_UserTags = nullptr;
-
-        jclass c_TagFields = nullptr;
-        jmethodID m_TagFields_Constructor = nullptr;
-        jfieldID f_TagFields_Artist = nullptr;
-        jfieldID f_TagFields_Length = nullptr;
-        jfieldID f_TagFields_Title = nullptr;
-
-        void init(JNIEnv* jni){
+        explicit JavaIDs_t(JNIEnv* jni) : jni(jni){
             c_ParseException = jni->FindClass("apps/chocolatecakecodes/bluebeats/taglib/ParseException");
 
             c_TagParser = jni->FindClass("apps/chocolatecakecodes/bluebeats/taglib/TagParser");
@@ -49,8 +26,40 @@ namespace {
             f_TagFields_Length = jni->GetFieldID(c_TagFields, "length", "J");
             f_TagFields_Title = jni->GetFieldID(c_TagFields, "title", "Ljava/lang/String;");
         }
+
+        JNIEnv* jni;
+
+        jclass c_ParseException = nullptr;
+
+        jclass c_TagParser = nullptr;
+        jfieldID f_TagParser_TagFields = nullptr;
+        jfieldID f_TagParser_UserTags = nullptr;
+
+        jclass c_TagFields = nullptr;
+        jmethodID m_TagFields_Constructor = nullptr;
+        jfieldID f_TagFields_Artist = nullptr;
+        jfieldID f_TagFields_Length = nullptr;
+        jfieldID f_TagFields_Title = nullptr;
+
+        jclass c_Chapter = nullptr;
+        jmethodID m_Chapter_Constructor = nullptr;
     };
-    struct JavaIDs_t JavaIDs;
+    typedef struct JavaIDs_t JavaIDs;
+
+    namespace BlueBeats {
+        // aliases for better type-checking
+        typedef jobject TagFields;
+        typedef jobject UserTags;
+        typedef jobject Chapters;
+
+        void throwParseException(const JavaIDs& jid, const char *msg);
+
+        TagFields readTags(const JavaIDs& jid, const File &file);
+
+        void readID3Tags(const JavaIDs& jid, const Tag *tag, const Properties* audioProps, TagFields dest);
+
+        Chapters readChapters(const JavaIDs& jid, const File& file);
+    }
 }
 //endregion
 
@@ -60,7 +69,7 @@ Java_apps_chocolatecakecodes_bluebeats_taglib_TagParser_parseMp3(
         JNIEnv* env,
         jobject thisRef,
         jstring filepath) {
-    JavaIDs.init(env);
+    JavaIDs jni(env);
 
     // create and parse file; copy java-string to cpp-string
     const char* pathChars = env->GetStringUTFChars(filepath, nullptr);
@@ -70,60 +79,59 @@ Java_apps_chocolatecakecodes_bluebeats_taglib_TagParser_parseMp3(
 
     // ensure the file was opened successful
     if(!file.isOpen()){
-        BlueBeats::throwParseException(env, "file could not be opened");
+        BlueBeats::throwParseException(jni, "file could not be opened");
         return;
     }
 
-    BlueBeats::TagFields tagFields = BlueBeats::readTags(env, file);
+    BlueBeats::TagFields tagFields = BlueBeats::readTags(jni, file);
     if(tagFields == nullptr) return;// an error occurred and so an exception was thrown
-    env->SetObjectField(thisRef, JavaIDs.f_TagParser_TagFields, tagFields);
+    env->SetObjectField(thisRef, jni.f_TagParser_TagFields, tagFields);
 }
 //endregion
 
 //region private methods
-void BlueBeats::throwParseException(JNIEnv* jni, const char* msg) {
-    jni->ThrowNew(JavaIDs.c_ParseException, msg);
+void BlueBeats::throwParseException(const JavaIDs& jid, const char* msg) {
+    jid.jni->ThrowNew(jid.c_ParseException, msg);
 }
 
-BlueBeats::TagFields BlueBeats::readTags(JNIEnv *jni, const File &file) {
+BlueBeats::TagFields BlueBeats::readTags(const JavaIDs& jid, const File &file) {
     // create new TagFields class
-    TagFields fields = jni->NewObject(JavaIDs.c_TagFields, JavaIDs.m_TagFields_Constructor);
+    TagFields fields = jid.jni->NewObject(jid.c_TagFields, jid.m_TagFields_Constructor);
     if(fields == nullptr){
-        throwParseException(jni, "unable to alloc object");
+        throwParseException(jid, "unable to alloc object");
         return nullptr;
     }
 
     Properties* audioProperties = file.audioProperties();
     Tag* id3Tag = file.tag();
     if(id3Tag != nullptr)
-        readID3Tags(jni, id3Tag, audioProperties, fields);
+        readID3Tags(jid, id3Tag, audioProperties, fields);
     //TODO optionally read APE tags
 
     return fields;
 }
 
-void BlueBeats::readID3Tags(JNIEnv *jni, const Tag *tag, const Properties* audioProps, TagFields dest) {
-    assert(jni != nullptr);
+void BlueBeats::readID3Tags(const JavaIDs& jid, const Tag *tag, const Properties* audioProps, TagFields dest) {
     assert(tag != nullptr);
     assert(dest != nullptr);
 
     String artist = tag->artist();
     if(!artist.isNull()){
         const char* artistStr = artist.toCString(true);
-        jstring artistJStr = jni->NewStringUTF(artistStr);
-        jni->SetObjectField(dest, JavaIDs.f_TagFields_Artist, artistJStr);
+        jstring artistJStr = jid.jni->NewStringUTF(artistStr);
+        jid.jni->SetObjectField(dest, jid.f_TagFields_Artist, artistJStr);
     }
 
     String title = tag->title();
     if(!title.isNull()){
         const char* titleStr = title.toCString(true);
-        jstring titleJStr = jni->NewStringUTF(titleStr);
-        jni->SetObjectField(dest, JavaIDs.f_TagFields_Title, titleJStr);
+        jstring titleJStr = jid.jni->NewStringUTF(titleStr);
+        jid.jni->SetObjectField(dest, jid.f_TagFields_Title, titleJStr);
     }
 
     if(audioProps != nullptr){
         jlong length = audioProps->lengthInMilliseconds();
-        jni->SetLongField(dest, JavaIDs.f_TagFields_Length, length);
+        jid.jni->SetLongField(dest, jid.f_TagFields_Length, length);
     }
 }
 //endregion
