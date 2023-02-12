f09ba18cc928d105552c456ca2d5504c1062ff56
blued_gear <chocolatecakecodes@disroot.org>
2023-02-12 15:57:41 +0800

    improves VlcPlayerManager

--- app/src/main/java/apps/chocolatecakecodes/bluebeats/media/VlcPlayerManager.kt
+++ app/src/main/java/apps/chocolatecakecodes/bluebeats/media/VlcPlayerManager.kt
@@ -1,18 +1,12 @@
 package apps.chocolatecakecodes.bluebeats.media
 
 import android.content.Context
-import android.view.View
-import android.view.ViewParent
 import android.widget.FrameLayout
 import apps.chocolatecakecodes.bluebeats.media.model.MediaFile
-import com.anggrayudi.storage.extension.launchOnUiThread
-import kotlinx.coroutines.delay
-import org.videolan.libvlc.FactoryManager
-import org.videolan.libvlc.Media
 import org.videolan.libvlc.MediaPlayer
 import org.videolan.libvlc.interfaces.*
 import org.videolan.libvlc.util.VLCVideoLayout
-import java.lang.IllegalStateException
+import kotlin.IllegalStateException
 
 /**
  * manages a player
@@ -21,7 +15,8 @@ class VlcPlayerManager(context: Context, private val mediaDB: MediaDB) {
 
     private val player: MediaPlayer
     private val playerView: VLCVideoLayout
-    private var currentPlayerViewHolder: FrameLayout? = null;
+    private var currentPlayerViewHolder: FrameLayout? = null
+    private var currentMedia: IMedia? = null
 
     init{
         player = MediaPlayer(VlcManagers.getLibVlc())
@@ -40,16 +35,39 @@ class VlcPlayerManager(context: Context, private val mediaDB: MediaDB) {
     fun detachPlayer(){
         if(isPlayerAttached())
             throw IllegalStateException("playerView is not attached")
+
         player.stop()
+
         currentPlayerViewHolder!!.removeView(playerView)
         currentPlayerViewHolder = null
+
+        val media = currentMedia
+        if(media !== null){
+            media.release()
+            currentMedia = null
+        }
     }
 
     fun isPlayerAttached(): Boolean{
         return currentPlayerViewHolder !== null
     }
 
-    fun play(media: MediaFile){
-        player.play(Media(libVlc, media.path))
+    fun play(mediaFile: MediaFile){
+        if(!isPlayerAttached())
+            throw IllegalStateException("player is not attached to a view")
+
+        val media = currentMedia
+        if(media !== null){
+            media.release()
+            currentMedia = null
+        }
+
+        val currentMedia = mediaDB.fileToVlcMedia(mediaFile.path)
+        if(currentMedia === null)
+            throw IllegalArgumentException("file could not be converted to media")
+        currentMedia.retain()
+        this.currentMedia = currentMedia
+
+        player.play(currentMedia)
     }
 }
