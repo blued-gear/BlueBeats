460660e953744a2ea0cb8fcc0a39c198f52b2bfe
blued_gear <chocolatecakecodes@disroot.org>
2023-02-12 15:57:43 +0800

    change some modifiers

--- app/src/main/java/apps/chocolatecakecodes/bluebeats/view/FileBrowserViewModel.kt
+++ app/src/main/java/apps/chocolatecakecodes/bluebeats/view/FileBrowserViewModel.kt
@@ -1,5 +1,6 @@
 package apps.chocolatecakecodes.bluebeats.view
 
+import android.os.Looper
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,16 +8,16 @@ import apps.chocolatecakecodes.bluebeats.media.VlcManagers
 import apps.chocolatecakecodes.bluebeats.media.model.MediaDir
 import apps.chocolatecakecodes.bluebeats.media.model.MediaFile
 
-class FileBrowserViewModel : ViewModel() {
+internal class FileBrowserViewModel : ViewModel() {
 
-    public val mediaDB = VlcManagers.getMediaDB()
-    public var mediaWasScanned: Boolean = false
+    val mediaDB = VlcManagers.getMediaDB()
+    var mediaWasScanned: Boolean = false
         private set
     private val currentDirRW = MutableLiveData<MediaDir>()
-    public val currentDir: LiveData<MediaDir> = currentDirRW
-    private val selectedFileRW = MutableLiveData<MediaFile>()
-    public val selectedFile: LiveData<MediaFile> = selectedFileRW
-    public val storagePermissionsGranted = MutableLiveData<Boolean>()
+    val currentDir: LiveData<MediaDir> = currentDirRW
+    private val selectedFileRW = MutableLiveData<MediaFile?>()
+    val selectedFile: LiveData<MediaFile?> = selectedFileRW
+    val storagePermissionsGranted = MutableLiveData<Boolean>()
 
     fun mediaScanned(){
         mediaWasScanned = true
