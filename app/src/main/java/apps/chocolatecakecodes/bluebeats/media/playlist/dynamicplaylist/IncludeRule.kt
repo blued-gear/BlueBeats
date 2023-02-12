7baa4d5a6045c0f325fb714b4bea44e1ec38993a
blued_gear <chocolatecakecodes@disroot.org>
2023-02-12 15:57:44 +0800

    [T61] implementation for IncludeRuleDao
    totally untested

--- app/src/main/java/apps/chocolatecakecodes/bluebeats/media/playlist/dynamicplaylist/IncludeRule.kt
+++ app/src/main/java/apps/chocolatecakecodes/bluebeats/media/playlist/dynamicplaylist/IncludeRule.kt
@@ -1,7 +1,11 @@
 package apps.chocolatecakecodes.bluebeats.media.playlist.dynamicplaylist
 
-import androidx.room.Dao
-import androidx.room.Transaction
+import androidx.room.*
+import apps.chocolatecakecodes.bluebeats.database.MediaDirDAO
+import apps.chocolatecakecodes.bluebeats.database.MediaDirEntity
+import apps.chocolatecakecodes.bluebeats.database.MediaFileDAO
+import apps.chocolatecakecodes.bluebeats.database.MediaFileEntity
+import apps.chocolatecakecodes.bluebeats.database.RoomDB
 import apps.chocolatecakecodes.bluebeats.media.model.MediaDir
 import apps.chocolatecakecodes.bluebeats.media.model.MediaFile
 import java.util.*
@@ -82,33 +86,191 @@ internal class IncludeRule private constructor(
     @Dao
     internal abstract class IncludeRuleDao {
 
+        private val fileDao: MediaFileDAO by lazy {
+            RoomDB.DB_INSTANCE.mediaFileDao()
+        }
+        private val dirDao: MediaDirDAO by lazy {
+            RoomDB.DB_INSTANCE.mediaDirDao()
+        }
+
+        //region api
+
         @Transaction
-        open fun createNew(): IncludeRule {
-            TODO()
+        open fun createNew(initialShare: Rule.Share): IncludeRule {
+            val id = insertEntity(IncludeRuleEntity(0, initialShare))
+            return IncludeRule(id, initialShare = initialShare)
         }
 
         fun load(id: Long): IncludeRule {
-            TODO()
+            val entity = getEntity(id)
+
+            val fileEntries = getFileEntriesForRule(id).map {
+                fileDao.getForId(it.file)
+            }.toSet()
+            val dirEntries = getDirEntriesForRule(id).map {
+                dirDao.getForId(it.dir) to it.deep
+            }.toSet()
+
+            return IncludeRule(id, dirEntries, fileEntries, entity.share)
         }
 
         @Transaction
         open fun save(rule: IncludeRule) {
-            TODO()
+            val currentFileEntries = generateFileEntries(rule).toSet()
+            val storedFileEntries = getFileEntriesForRule(rule.entityId)
+                .map { it.copy(id = 0) }// set id to 0 to be comparable to generated entries
+                .toSet()
+
+            storedFileEntries.minus(currentFileEntries).forEach {
+                deleteFileEntry(it.rule, it.file)
+            }
+            currentFileEntries.minus(storedFileEntries).forEach {
+                insertFileEntry(it)
+            }
+
+            val currentDirEntries = generateDirEntries(rule).toSet()
+            val storedDirEntries = getDirEntriesForRule(rule.entityId)
+                .map { it.copy(id = 0) }// set id to 0 to be comparable to generated entries
+                .toSet()
+
+            storedDirEntries.minus(currentDirEntries).forEach {
+                deleteDirEntry(it.rule, it.dir)
+            }
+            currentDirEntries.minus(storedDirEntries).forEach {
+                insertDirEntry(it)
+            }
+
+            updateEntity(IncludeRuleEntity(rule.entityId, rule.share))
         }
 
         @Transaction
         open fun delete(rule: IncludeRule) {
-            TODO()
+            deleteAllFileEntries(rule.entityId)
+            deleteAllDirEntries(rule.entityId)
+            deleteEntity(rule.entityId)
         }
 
         fun getEntityId(rule: IncludeRule): Long {
             return rule.entityId
         }
+        //endregion
 
-        //TODO
+        //region sql
+
+        @Insert
+        protected abstract fun insertEntity(entity: IncludeRuleEntity): Long
+
+        @Update
+        protected abstract fun updateEntity(entity: IncludeRuleEntity)
+
+        @Query("DELETE FROM IncludeRuleEntity WHERE id = :id;")
+        protected abstract fun deleteEntity(id: Long)
+
+        @Query("SELECT * FROM IncludeRuleEntity WHERE id = :id;")
+        protected abstract fun getEntity(id: Long): IncludeRuleEntity
+
+        @Insert
+        protected abstract fun insertFileEntry(entry: IncludeRuleFileEntry): Long
+
+        @Query("DELETE FROM IncludeRuleFileEntry WHERE rule = :rule;")
+        protected abstract fun deleteAllFileEntries(rule: Long)
+
+        @Query("DELETE FROM IncludeRuleFileEntry WHERE rule = :rule AND file = :file;")
+        protected abstract fun deleteFileEntry(rule: Long, file: Long)
+
+        @Query("SELECT * FROM IncludeRuleFileEntry WHERE rule = :rule;")
+        protected abstract fun getFileEntriesForRule(rule: Long): List<IncludeRuleFileEntry>
+
+        @Insert
+        protected abstract fun insertDirEntry(entry: IncludeRuleDirEntry): Long
+
+        @Query("DELETE FROM IncludeRuleDirEntry WHERE rule = :rule;")
+        protected abstract fun deleteAllDirEntries(rule: Long)
+
+        @Query("DELETE FROM IncludeRuleDirEntry WHERE rule = :rule AND dir = :dir;")
+        protected abstract fun deleteDirEntry(rule: Long, dir: Long)
+
+        @Query("SELECT * FROM IncludeRuleDirEntry WHERE rule = :rule;")
+        protected abstract fun getDirEntriesForRule(rule: Long): List<IncludeRuleDirEntry>
+        //endregion
+
+        //region private helpers
+
+        private fun generateFileEntries(rule: IncludeRule): List<IncludeRuleFileEntry> {
+            return rule.getFiles().map {
+                IncludeRuleFileEntry(
+                    0,
+                    rule.entityId,
+                    it.entity.id
+                )
+            }
+        }
+
+        private fun generateDirEntries(rule: IncludeRule): List<IncludeRuleDirEntry> {
+            return rule.getDirs().map {
+                IncludeRuleDirEntry(
+                    0,
+                    rule.entityId,
+                    it.first.entity.id,
+                    it.second
+                )
+            }
+        }
+        //endregion
     }
 }
 
 //region entities
+@Entity
+internal data class IncludeRuleEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    @Embedded(prefix = "share_") val share: Rule.Share
+)
 
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = IncludeRuleEntity::class,
+            parentColumns = ["id"], childColumns = ["rule"],
+            onDelete = ForeignKey.RESTRICT, onUpdate = ForeignKey.RESTRICT
+        ),
+        ForeignKey(
+            entity = MediaFileEntity::class,
+            parentColumns = ["id"], childColumns = ["file"],
+            onDelete = ForeignKey.CASCADE, onUpdate = ForeignKey.CASCADE
+        ),
+    ],
+    indices = [
+        Index(value = ["rule", "file"], unique = true)
+    ]
+)
+internal data class IncludeRuleFileEntry(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    val rule: Long,
+    val file: Long
+)
+
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = IncludeRuleEntity::class,
+            parentColumns = ["id"], childColumns = ["rule"],
+            onDelete = ForeignKey.RESTRICT, onUpdate = ForeignKey.RESTRICT
+        ),
+        ForeignKey(
+            entity = MediaDirEntity::class,
+            parentColumns = ["id"], childColumns = ["dir"],
+            onDelete = ForeignKey.CASCADE, onUpdate = ForeignKey.CASCADE
+        ),
+    ],
+    indices = [
+        Index(value = ["rule", "dir"], unique = true)
+    ]
+)
+internal data class IncludeRuleDirEntry(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    val rule: Long,
+    val dir: Long,
+    val deep: Boolean
+)
 //endregion
