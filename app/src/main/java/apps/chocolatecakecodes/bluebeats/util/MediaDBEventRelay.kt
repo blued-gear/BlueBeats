8794ff5c9a2635ac86d646d399c0a7f520d509e7
blued_gear <chocolatecakecodes@disroot.org>
2023-02-12 15:57:41 +0800

    new class to broadcast MediaDB events to multiple listeners
    issue: T30

---
+++ app/src/main/java/apps/chocolatecakecodes/bluebeats/util/MediaDBEventRelay.kt
@@ -0,0 +1,83 @@
+package apps.chocolatecakecodes.bluebeats.util
+
+import apps.chocolatecakecodes.bluebeats.media.MediaDB
+import apps.chocolatecakecodes.bluebeats.media.model.MediaNode
+import java.util.concurrent.atomic.AtomicReference
+
+class MediaDBEventRelay() : MediaDB.ScanEventHandler(null) {
+
+    private val subscribers = mutableListOf<MediaDB.ScanEventHandler>()
+    private var subject: AtomicReference<MediaDB> = AtomicReference(null)
+
+    //region public methods
+
+    fun setSubject(mediaDB: MediaDB){
+        if(subject.get() !== null)
+            throw IllegalStateException("subject already set")
+        subject.set(mediaDB)
+    }
+    fun getSubject(): MediaDB{
+        val subject = this.subject.get()
+        if(subject === null)
+            throw IllegalStateException("subject not set")
+        return subject
+    }
+
+    fun addSubscriber(handler: MediaDB.ScanEventHandler){
+        synchronized(this) {
+            subscribers.add(handler)
+        }
+    }
+    fun removeSubscriber(handler: MediaDB.ScanEventHandler){
+        synchronized(this) {
+            subscribers.remove(handler)
+        }
+    }
+    //endregion
+
+    // region relay methods
+
+    override fun handleScanStarted() {
+        synchronized(this){
+            subscribers.forEach {
+                it.onScanStarted()
+            }
+        }
+    }
+    override fun handleScanFinished() {
+        synchronized(this){
+            subscribers.forEach {
+                it.onScanFinished()
+            }
+        }
+    }
+    override fun handleNewNodeFound(node: MediaNode) {
+        synchronized(this){
+            subscribers.forEach {
+                it.onNewNodeFound(node)
+            }
+        }
+    }
+    override fun handleNodeRemoved(node: MediaNode) {
+        synchronized(this){
+            subscribers.forEach {
+                it.onNodeRemoved(node)
+            }
+        }
+    }
+    override fun handleNodeUpdated(node: MediaNode, oldVersion: MediaNode) {
+        synchronized(this){
+            subscribers.forEach {
+                it.onNodeUpdated(node, oldVersion)
+            }
+        }
+    }
+    override fun handleScanException(e: Exception) {
+        synchronized(this){
+            subscribers.forEach {
+                it.onScanException(e)
+            }
+        }
+    }
+    //endregion
+}
